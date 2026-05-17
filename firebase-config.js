e3dc11484793bfa457d3e2cbf5991565a1ac442d
dayxnight <dayxnight@example.com>
2026-05-18 03:17:53 +0700

    feat: Integrate Firebase Analytics and centralize configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
 
 // Konfigurasi Firebase
 const firebaseConfig = {
@@ -10,10 +11,12 @@ const firebaseConfig = {
     projectId: "t-chat-db",
     storageBucket: "t-chat-db.firebasestorage.app",
     messagingSenderId: "656960746695",
-    appId: "1:656960746695:web:7b06a7784149d3e7747ae2"
+    appId: "1:656960746695:web:7b06a7784149d3e7747ae2",
+    measurementId: "G-SBWNHZYFZ9"
 };
 
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export const analytics = getAnalytics(app);
